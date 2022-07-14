140b3454421ad761f2cd0bb2139263777f743a15
drd4games <tsenisda@gmail.com>
2022-07-15 01:12:33 +0300

    test

--- src/App.js
+++ src/App.js
@@ -10,92 +10,89 @@ import { useEffect, useState } from 'react';
 // https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3
 // https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3
 
+const keyDownHander = (e) => {
+  switch (e.code) {
+    case 'KeyQ':
+      document.getElementById(buttons[0].id).click();
+      break;
+    case 'KeyW':
+      document.getElementById(buttons[1].id).click();
+      break;
+    case 'KeyE':
+      document.getElementById(buttons[2].id).click();
+      break;
+    case 'KeyA':
+      document.getElementById(buttons[3].id).click();
+      break;
+    case 'KeyS':
+      document.getElementById(buttons[4].id).click();
+      break;
+    case 'KeyD':
+      document.getElementById(buttons[5].id).click();
+      break;
+    case 'KeyZ':
+      document.getElementById(buttons[6].id).click();
+      break;
+    case 'KeyX':
+      document.getElementById(buttons[7].id).click();
+      break;
+    case 'KeyC':
+      document.getElementById(buttons[8].id).click();
+      break;
+
+    default:
+      break;
+  }
+};
+const buttons = [
+  {
+    id: 'Heater-1',
+    name: 'q',
+    sound: '1'
+  },
+  {
+    id: 'Heater-2',
+    name: 'w',
+    sound: '2'
+  },
+  {
+    id: 'Heater-3',
+    name: 'e',
+    sound: '2'
+  },
+  {
+    id: 'Heater-4_1',
+    name: 'a',
+    sound: '2'
+  },
+  {
+    id: 'Heater-6',
+    name: 's',
+    sound: '2'
+  },
+  {
+    id: 'Dsc_Oh',
+    name: 'd',
+    sound: '2'
+  },
+  {
+    id: 'Kick_n_Hat',
+    name: 'z',
+    sound: '2'
+  },
+  {
+    id: 'RP4_KICK_1',
+    name: 'x',
+    sound: '2'
+  },
+  {
+    id: 'Cev_H2',
+    name: 'c',
+    sound: '2'
+  }
+];
 function App() {
-  const buttons = [
-    {
-      id: 'Heater-1',
-      name: 'q',
-      sound: '1'
-    },
-    {
-      id: 'Heater-2',
-      name: 'w',
-      sound: '2'
-    },
-    {
-      id: 'Heater-3',
-      name: 'e',
-      sound: '2'
-    },
-    {
-      id: 'Heater-4_1',
-      name: 'a',
-      sound: '2'
-    },
-    {
-      id: 'Heater-6',
-      name: 's',
-      sound: '2'
-    },
-    {
-      id: 'Dsc_Oh',
-      name: 'd',
-      sound: '2'
-    },
-    {
-      id: 'Kick_n_Hat',
-      name: 'z',
-      sound: '2'
-    },
-    {
-      id: 'RP4_KICK_1',
-      name: 'x',
-      sound: '2'
-    },
-    {
-      id: 'Cev_H2',
-      name: 'c',
-      sound: '2'
-    }
-  ];
-
   const [soundDisplay, setSoundDisplay] = useState('');
-
-  const keyDownHander = (e) => {
-    switch (e.code) {
-      case 'KeyQ':
-        document.getElementById(buttons[0].id).click();
-        break;
-      case 'KeyW':
-        document.getElementById(buttons[1].id).click();
-        break;
-      case 'KeyE':
-        document.getElementById(buttons[2].id).click();
-        break;
-      case 'KeyA':
-        document.getElementById(buttons[3].id).click();
-        break;
-      case 'KeyS':
-        document.getElementById(buttons[4].id).click();
-        break;
-      case 'KeyD':
-        document.getElementById(buttons[5].id).click();
-        break;
-      case 'KeyZ':
-        document.getElementById(buttons[6].id).click();
-        break;
-      case 'KeyX':
-        document.getElementById(buttons[7].id).click();
-        break;
-      case 'KeyC':
-        document.getElementById(buttons[8].id).click();
-        break;
-
-      default:
-        break;
-    }
-    console.log(e);
-  };
 
   useEffect(() => {
     document.addEventListener('keydown', keyDownHander);
